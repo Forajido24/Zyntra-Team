0d929e42cd7029180afbfec81607e109c76ec752
AngelForajido <159275784+Forajido24@users.noreply.github.com>
2026-03-14 20:19:29 -0600

    Update lib.rs

--- Docker/Docker-worker/shared/src/lib.rs
+++ Docker/Docker-worker/shared/src/lib.rs
@@ -1,6 +1,6 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Task {
     pub task_id: u32,
     pub row: u32,
@@ -9,10 +9,19 @@ pub struct Task {
     pub max_iter: u32,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TaskResult {
     pub task_id: u32,
     pub worker_id: String,
     pub row: u32,
     pub data: Vec<u32>,
+    pub latency_ms: u64, // <--- ESTO ES LO QUE NO ENCUENTRA EL COMPILADOR
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LogMessage {
+    pub source: String,
+    pub level: String,
+    pub message: String,
+    pub timestamp: u64,
 }
